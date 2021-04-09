8172f9ddb5fa9c0ba1895d9cde0e5d8c2e74fec2
jessicacordeiro <jessica.cordeiro121@gmail.com>
2021-04-09 14:07:52 -0300

    Implementação do put-user

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -33,6 +33,9 @@ export class InicioComponent implements OnInit {
   ) { }
 
   ngOnInit() {
+
+    window.scroll(0,0)
+
     if(environment.token == ''){
       this.router.navigate (['/entrar'])
     }
